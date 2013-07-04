0cfebfccbf735f6f901c12e5d92a4fd461bd6e6c
stijnvanbael <stijnvanbael@gmail.com>
2013-07-04 21:44:56 +0200

    whoops

--- appify-framework/appify-framework-common/src/test/java/be/appify/framework/common/security/service/Sha1EncryptionServiceTest.java
+++ appify-framework/appify-framework-common/src/test/java/be/appify/framework/common/security/service/Sha1EncryptionServiceTest.java
@@ -3,11 +3,8 @@ package be.appify.framework.common.security.service;
 public class Sha1EncryptionServiceTest {
 
 	public static void main(String[] args) {
-		Sha1EncryptionService encryptionService = new Sha1EncryptionService("imagine512", "UTF-8");
-		System.out.println(encryptionService.encrypt("tim.coremans"));
-		System.out.println(encryptionService.encrypt("stijn.van.bael"));
-		System.out.println(encryptionService.encrypt("pieter.van.bael"));
-		System.out.println(encryptionService.encrypt("geert.van.linden"));
+		Sha1EncryptionService encryptionService = new Sha1EncryptionService("salt", "UTF-8");
+		System.out.println(encryptionService.encrypt("password"));
 	}
 
 }
